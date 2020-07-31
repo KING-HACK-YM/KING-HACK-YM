20ff838ceb093975dbc82286b267d154e62594c2
KING-HACK-YM <69021950+KING-HACK-YM@users.noreply.github.com>
2020-07-31 04:11:44 +0300

    Add files via upload

---
+++ KING-HACK-YM.sh
@@ -0,0 +1,103 @@
+green='\e[1;32m'
+red='\e[1;31m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;35m'
+yellow='\e[1;33m'
+clear
+termux-open https://www.youtube.com/channel/UC_tCv91xx-MBXAlE7sjIFBA
+echo -e $green '──────────────────────────────────────────────'
+echo -e $red '╔╦╗╔══╗╔═╦╗╔══╗────╔╗╔╗╔══╗╔═╗╔╦╗────╔═╦╗╔═╦═╗'
+echo -e $red '║╔╝╚║║╝║║║║║╔═╣╔══╗║╚╝║║╔╗║║╔╝║╔╝╔══╗╚╗║║║║║║║'
+echo -e $red '║╚╗╔║║╗║║║║║╚╗║╚══╝║╔╗║║╠╣║║╚╗║╚╗╚══╝╔╩╗║║║║║║'
+echo -e $red '╚╩╝╚══╝╚╩═╝╚══╝────╚╝╚╝╚╝╚╝╚═╝╚╩╝────╚══╝╚╩═╩╝'
+echo -e $green '──────────────────────────────────────────────'
+echo -e $red '+ YouTube:https://www.youtube.com/channel/UC_tCv91xx-MBXAlE7sjIFBA'
+echo -e $red '+ Telegram:https://t.me/kinghackym'
+echo -e $red '+ Whatsapp: +967777255026'
+echo
+echo
+echo -e $red 'Start Install....{git}' $green
+pkg install git -y
+echo -e $red 'Finsh Install {git}' $green
+echo
+echo -e $red 'Start Install....{python 1/2/3}' $green
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+echo -e $red 'Finsh Install {python}' $green
+echo
+echo -e $red 'Start Install....{figlet}' $green
+pkg install figlet -y
+echo -e $red 'Finsh Install {figlet}' $green
+echo
+echo -e $red 'Finsh Install {nano / wget}' $green
+pkg install nano -y
+pkg install wget -y
+echo -e $red 'Finsh Install {nano / wget}' $green
+echo
+echo -e $red 'Start Install....{ruby}' $green
+pkg install ruby -y
+echo -e $red 'Finsh Install {ruby}' $green
+echo
+echo -e $red 'Start Install....{php}' $green
+pkg install php -y
+echo -e $red 'Finsh Install {php}' $green
+echo
+echo -e $red 'Start Install....{toilet}' $green
+pkg install toilet -y
+echo -e $red 'Finsh Install {toilet}' $green
+echo
+echo -e $red 'Start Install....{unzip / w3m / perl}' $green
+pkg install perl -y
+pkg install unzip -y
+pkg install w3m -y
+echo -e $red 'Finsh Install {unzip / w3m / perl}' $green
+echo
+echo -e $red 'Start Install....{curl}' $green
+pkg install curl -y
+echo -e $red 'Finsh Install {curl}' $green
+apt update -y
+apt upgrade -y
+echo -e $red
+read -p 'Finsh Install All Pkgs' $green
+pkg update && pkg upgrade -y
+pkg install python
+pkg install pytho2
+pkg install python3
+pkg install bash
+pkg install php
+pkg install git
+pkg install wget
+pkg install curl
+pkg install ruby
+pkg install unzip
+pkg install nano
+pkg install proot
+pkg install openssh
+pkg install pip
+pkg install pip2
+termux-chroot
+pip install mechanez
+pip install requestes
+echo '──────────────────────────────────────────────'
+echo '╔╦╗╔══╗╔═╦╗╔══╗────╔╗╔╗╔══╗╔═╗╔╦╗────╔═╦╗╔═╦═╗'
+echo '║╔╝╚║║╝║║║║║╔═╣╔══╗║╚╝║║╔╗║║╔╝║╔╝╔══╗╚╗║║║║║║║'
+echo '║╚╗╔║║╗║║║║║╚╗║╚══╝║╔╗║║╠╣║║╚╗║╚╗╚══╝╔╩╗║║║║║║'
+echo '╚╩╝╚══╝╚╩═╝╚══╝────╚╝╚╝╚╝╚╝╚═╝╚╩╝────╚══╝╚╩═╩╝'
+echo '──────────────────────────────────────────────'
+echo '# My Name: KING-HACK-YM'
+echo '# Surname: KING-HACK-YM'
+echo '# My Age: 16 Years old'
+echo '# From: Yemen/Sana,a'
+echo '# Skills: Programmer Bash and Hacker'
+echo '↓↓↓↓↓↓↓↓↓↓↓ ↓↓↓↓↓↓↓↓↓↓↓↓↓↓'
+echo '+ YouTube:https://www.youtube.com/channel/UC_tCv91xx-MBXAlE7sjIFBA'
+echo '+ Telegram:https://t.me/kinghackym'
+echo '+ Whatsapp: +967777255026'
+echo '+ Github: github.com/KING-HACK-YM'
+echo -e $red'<===================================>'
+echo -e $green'            Good Luck                '
+echo -e $red'<===================================>'
+
